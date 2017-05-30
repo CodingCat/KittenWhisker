99623a3acdc9c4c6849fa54ad6b4f43be0c3fca2
Nan Zhu <zhunansjtu@gmail.com>
2017-05-30 11:07:53 -0700

    test perf agent premain

--- src/main/java/me/codingcat/happysparking/PerfAgent.java
+++ src/main/java/me/codingcat/happysparking/PerfAgent.java
@@ -9,22 +9,14 @@ import com.sun.tools.attach.VirtualMachine;
 
 public class PerfAgent {
 
-  public static final String PERFJ_SYSTEM_PROPERTIES_FILE = "info-minzhou-perfj.properties";
-  public static final String KEY_PERFJ_LIB_PATH = "info.minzhou.perfj.lib.path";
-  public static final String KEY_PERFJ_LIB_NAME = "info.minzhou.perfj.lib.name";
-  public static final String KEY_PERFJ_TEMPDIR = "info.minzhou.perfj.tempdir";
-  public static final String KEY_PERFJ_USE_SYSTEMLIB = "info.minzhou.perfj.use.systemlib";
-
   private static boolean hasResource(String path) {
     return PerfAgent.class.getResource(path) != null;
   }
 
-  private static File extractLibraryFile(String libraryFolder, String libraryFileName, String targetFolder) {
-    String nativeLibraryFilePath = libraryFolder + "/" + libraryFileName;
+  private static File extractLibraryFile(String libraryFileName, String targetFolder) {
 
-    // Attach UUID to the native library file to ensure multiple processes can read the libperfj multiple times.
     String uuid = UUID.randomUUID().toString();
-    String extractedLibFileName = String.format("perfj-%s-%s", uuid, libraryFileName);
+    String extractedLibFileName = String.format("happysparking-%s-%s", uuid, libraryFileName);
     File extractedLibFile = new File(targetFolder, extractedLibFileName);
 
     try {
@@ -32,7 +24,7 @@ public class PerfAgent {
       InputStream reader = null;
       FileOutputStream writer = null;
       try {
-        reader = PerfAgent.class.getResourceAsStream(nativeLibraryFilePath);
+        reader = PerfAgent.class.getResourceAsStream(libraryFileName);
         try {
           writer = new FileOutputStream(extractedLibFile);
 
@@ -68,7 +60,7 @@ public class PerfAgent {
         InputStream nativeIn = null;
         InputStream extractedLibIn = null;
         try {
-          nativeIn = PerfAgent.class.getResourceAsStream(nativeLibraryFilePath);
+          nativeIn = PerfAgent.class.getResourceAsStream(libraryFileName);
           extractedLibIn = new FileInputStream(extractedLibFile);
 
           if (!contentsEquals(nativeIn, extractedLibIn)) {
@@ -113,61 +105,39 @@ public class PerfAgent {
   }
 
   private static File findNativeLibrary() {
-    boolean useSystemLib = Boolean.parseBoolean(System.getProperty(KEY_PERFJ_USE_SYSTEMLIB, "false"));
-    if (useSystemLib) {
-      return null; // Use a pre-installed libperfj
-    }
-
-    // Try to load the library in info.minzhou.perfj.lib.path  */
-    String perfjNativeLibraryPath = System.getProperty(KEY_PERFJ_LIB_PATH);
-    String perfjNativeLibraryName = System.getProperty(KEY_PERFJ_LIB_NAME);
-
-    // Resolve the library file name with a suffix (e.g., dll, .so, etc.)
-    if (perfjNativeLibraryName == null) {
-      perfjNativeLibraryName = System.mapLibraryName("perfj");
-    }
-
-    if (perfjNativeLibraryPath != null) {
-      File nativeLib = new File(perfjNativeLibraryPath, perfjNativeLibraryName);
-      if (nativeLib.exists()) {
-        return nativeLib;
-      }
-    }
-
     // Load native library inside a jar file
-    perfjNativeLibraryPath = "/info/minzhou/perfj/native";
-    boolean hasNativeLib = hasResource(perfjNativeLibraryPath + "/" + perfjNativeLibraryName);
+    String perfjNativeLibraryPath = "libperfmap.so";
+    boolean hasNativeLib = hasResource("libperfmap.so");
 
     if (!hasNativeLib) {
       throw new RuntimeException("no native library is found for perfj");
     }
 
     // Temporary folder for the native lib. Use the value of info.minzhou.perfj.tempdir or java.io.tmpdir
-    File tempFolder = new File(System.getProperty(KEY_PERFJ_TEMPDIR, System.getProperty("java.io.tmpdir")));
+    File tempFolder = new File(System.getProperty(System.getProperty("java.io.tmpdir")));
     if (!tempFolder.exists()) {
       boolean created = tempFolder.mkdirs();
       if (!created) {
         // if created == false, it will fail eventually in the later part
       }
     }
-
     // Extract and load a native library inside the jar file
-    return extractLibraryFile(perfjNativeLibraryPath, perfjNativeLibraryName, tempFolder.getAbsolutePath());
+    return extractLibraryFile(perfjNativeLibraryPath, tempFolder.getAbsolutePath());
   }
 
-  // for running after VM start
+  // TODO: for running after VM start
   public static void agentmain(final String args, final Instrumentation instrumentation) {
     // int passedInPID = Integer.valueOf(args[0])
   }
 
   public static void premain(final String args, final Instrumentation instrumentation) {
     try {
-      /*
+      File f = findNativeLibrary();
+      String options = args;
       String currentVMPID = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
       // attach to self
       VirtualMachine vm = VirtualMachine.attach(currentVMPID);
-      vm.loadAgentPath();
-      */
+      vm.loadAgentPath(f.getAbsolutePath(), options);
     } catch (Exception e) {
       e.printStackTrace();
     }
