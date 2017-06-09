61d069068eca614795ff663b430c66b0b1ac4380
Nan Zhu <zhunansjtu@gmail.com>
2017-06-09 10:59:11 -0700

    give nopasswd permission of chmod to yarn

--- start_test.sh
+++ start_test.sh
@@ -8,7 +8,7 @@ NODES=`cat slaves`
 
 for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
     echo $slave_ip
-    ssh $username@$slave_ip "echo 'yarn ALL=(ALL) NOPASSWD:/usr/bin/perf' | sudo tee --append /etc/sudoers.d/yarn"
+    ssh $username@$slave_ip "echo 'yarn ALL=(ALL) NOPASSWD:/usr/bin/perf,/bin/chmod' | sudo tee --append /etc/sudoers.d/yarn"
 done
 
 . spark_app_cmd.sh
