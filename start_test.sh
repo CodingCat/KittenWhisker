4da01482f41e9d2e99d91b4351378c5c2608e6f1
Nan Zhu <zhunansjtu@gmail.com>
2017-06-09 11:54:29 -0700

    download symbol and perf record files automaticallyh

--- start_test.sh
+++ start_test.sh
@@ -4,8 +4,13 @@ set -x
 
 username="$1"
 
+shared_directory = "$2"
+
+local_directory = "$3"
+
 NODES=`cat slaves`
 
+# add permission for yarn to run perf
 for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
     echo $slave_ip
     ssh $username@$slave_ip "echo 'yarn ALL=(ALL) NOPASSWD:/usr/bin/perf,/bin/chmod' | sudo tee --append /etc/sudoers.d/yarn"
@@ -13,7 +18,11 @@ done
 
 . spark_app_cmd.sh
 
+# revoke permission of yarn
 for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
     echo $slave_ip
     ssh $username@$slave_ip "sudo rm /etc/sudoers.d/yarn"
 done
+
+# collect files to local
+hdfs dfs -get $shared_directory $local_directory
