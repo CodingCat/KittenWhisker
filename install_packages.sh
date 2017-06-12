97f2e555f24f941865ef088be7683e43bc4736b0
Nan Zhu <zhunansjtu@gmail.com>
2017-06-12 11:28:12 -0700

    test

--- install_packages.sh
+++ install_packages.sh
@@ -9,7 +9,7 @@ NODES=`cat slaves`
 # add permission for yarn to run perf
 for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
     echo $slave_ip
-    ssh $username@$slave_ip "apt-get install linux-tools-common linux-tools-generic linux-tools-`uname -r`"
+    ssh $username@$slave_ip "sudo apt-get install linux-tools-common linux-tools-generic linux-tools-`uname -r`"
     ssh $username@$slave_ip "sudo apt-get install openjdk-8-dbg"
 done
 
