4314038ec95f5edf6ab0bf6c80f0d63afa0ef4c8
CodingCat <zhunansjtu@gmail.com>
2017-08-30 15:49:09 -0700

    fix script

--- generate_flamegraph.sh
+++ generate_flamegraph.sh
@@ -4,14 +4,19 @@ set -x
 
 local_directory="$1"
 
+# generate stack
 java -cp target/happysparking-0.1-SNAPSHOT-jar-with-dependencies.jar me.codingcat.happysparking.StackTraceGenerator $local_directory
 
-# generate graph
+if [[ -z $FLAMEGRAPH_DIR ]]; then
+    echo "you have to define FLAMEGRAPH_DIR";
+    exit 1
+fi
 
+# generate graph
 for file in $local_directory
 do
     if [[ $file =~ .*\.stack ]]; then
         echo $file
-        # $FLAMEGRAPH_DIR/stackcollapse-perf.pl $PERF_COLLAPSE_OPTS $file | tee $COLLAPSED | $FLAMEGRAPH_DIR/flamegraph.pl $PERF_FLAME_OPTS > $PERF_FLAME_OUTPUT
+        $FLAMEGRAPH_DIR/stackcollapse-perf.pl $PERF_COLLAPSE_OPTS $file | tee $file.collapsed | $FLAMEGRAPH_DIR/flamegraph.pl --color=java > $file.svg
     fi
 done
