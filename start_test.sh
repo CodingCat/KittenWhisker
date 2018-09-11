22b856b10d17106bb495cd9ac4785b577b42ced4
Nan Zhu <nanzhu@uber.com>
2018-09-11 14:31:44 -0700

    don't touch yarn permission when running test

--- start_test.sh
+++ start_test.sh
@@ -2,27 +2,13 @@
 
 set -x
 
-username="$1"
+shared_directory="$1"
 
-shared_directory="$2"
-
-local_directory="$3"
+local_directory="$2"
 
 NODES=`cat slaves`
 
-# add permission for yarn to run perf
-for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
-    echo $slave_ip
-    ssh $username@$slave_ip "echo 'yarn ALL=(ALL) NOPASSWD:/usr/bin/perf,/bin/chmod' | sudo tee --append /etc/sudoers.d/yarn"
-done
-
 . spark_app_cmd.sh
-
-# revoke permission of yarn
-for slave_ip in `echo $NODES|sed  "s/#.*$//;/^$/d"`; do
-    echo $slave_ip
-    ssh $username@$slave_ip "sudo rm /etc/sudoers.d/yarn"
-done
 
 # collect files to local
 hdfs dfs -get $shared_directory $local_directory
