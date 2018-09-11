81df1fb42ddf0ee65703761834ce6f69b36fd638
Nan Zhu <nanzhu@uber.com>
2018-09-11 14:41:37 -0700

    merge

--- start_test.sh
+++ start_test.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 shared_directory="$1"
 
 local_directory="$2"
 
-. spark_app_cmd.sh
+. $SCRIPT_DIR/spark_app_cmd.sh
 
 # collect files to local
 hdfs dfs -get $shared_directory $local_directory
